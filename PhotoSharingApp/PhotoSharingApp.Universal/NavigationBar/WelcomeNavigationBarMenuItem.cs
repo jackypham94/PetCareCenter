4cd60cc0c02f52227780b67c4d08a1ca47119f2f
jackypham94 <jackypham94@outlook.com>
2016-05-28 15:25:34 +0700

    Upload
    Upload new project

---
+++ PhotoSharingApp/PhotoSharingApp.Universal/NavigationBar/WelcomeNavigationBarMenuItem.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using PhotoSharingApp.Universal.Views;
+using Windows.UI.Xaml.Controls;
+
+namespace PhotoSharingApp.Universal.NavigationBar
+{
+    /// <summary>
+    /// Navigation bar menu item that navigates to the
+    /// <see cref="WelcomePage" />.
+    /// </summary>
+    public class WelcomeNavigationBarMenuItem : NavigationBarMenuItemBase,
+        INavigationBarMenuItem
+    {
+        /// <summary>
+        /// Gets the type of the destination page.
+        /// </summary>
+        public Type DestPage
+        {
+            get { return typeof(WelcomePage); }
+        }
+
+        /// <summary>
+        /// Gets the title displayed in the navigation bar.
+        /// </summary>
+        public string Label
+        {
+            get { return "Instructions"; }
+        }
+
+        /// <summary>
+        /// Gets the symbol that is displayed in the navigation bar.
+        /// </summary>
+        public override Symbol Symbol
+        {
+            get { return Symbol.Help; }
+        }
+    }
+}
---
+++ PhotoSharingApp/PhotoSharingApp.Universal/Views/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Practices.ServiceLocation;
+using PhotoSharingApp.Universal.ViewModels;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PhotoSharingApp.Universal.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly MainPageViewModel _viewModel;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _viewModel = ServiceLocator.Current.GetInstance<MainPageViewModel>();
+            DataContext = _viewModel;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await _viewModel.LoadState();
+        }
+    }
+}
